888f3251230a2491594103a2071aecdc88825b86
mkdusia <madziakycler@gmail.com>
2026-03-30 00:51:07 +0200

    Add basic page layout

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,7 +3,22 @@ import './App.css'
 function App() {
   return (
     <div className="app-container">
-      Tupitru!
+      <h1 className='title'>Tupitru!</h1>
+      <div className='wrapper'>
+        <label className='label'>Nickname: </label>
+        <input name="nick" className='inputtext'/>
+        
+        <label className='label'>Room code:</label> 
+        <input name="room" className='inputtext'/>
+        
+        <button className='button'>
+          Join
+        </button>
+      
+        <h2>.</h2>
+        <h4>Want to start a new game?</h4>
+        <button className='button'>Create room</button>
+      </div>
     </div>
   )
 }
