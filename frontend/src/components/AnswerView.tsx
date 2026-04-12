7aae0ef179c3a3b1d80d6b76cae5a333c2b1ab17
k-kosiorowski <krzysztof.kosiorowski@student.uj.edu.pl>
2026-04-13 00:10:10 +0200

    Added current answer info

--- frontend/src/components/AnswerView.tsx
+++ frontend/src/components/AnswerView.tsx
@@ -2,15 +2,20 @@ import '../App.css'
 import TupitruTitle from './Title';
 
 interface AnswerViewProps {
+  current_answer: number;
   setAnswer: (answer: number) => void;
   handleSendAnswer: () => void;
 }
 
-function AnswerView({setAnswer, handleSendAnswer} : AnswerViewProps) {
+function AnswerView({current_answer, setAnswer, handleSendAnswer} : AnswerViewProps) {
 return (
       <div className="app-container">
         <TupitruTitle/>
         <div className='wrapper'>
+          <h2> Your current answer: </h2>
+          { current_answer > 0 && (
+                <span className="num">{current_answer}</span>
+          )}
           <label className='label'>Answer: </label>
           <input 
             name="nick" 
