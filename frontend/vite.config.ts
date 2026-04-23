1731576e99cbaa0edf1f64c97cab2a5425b60756
k-kosiorowski <krzysztof.kosiorowski@student.uj.edu.pl>
2026-04-23 19:22:06 +0200

    Moved Phaser to a separate chunk in vite.config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,4 +4,23 @@ import react from '@vitejs/plugin-react';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    chunkSizeWarningLimit: 1500,
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules/phaser')) {
+            return 'phaser';
+          }
+          if (
+            id.includes('node_modules/react') ||
+            id.includes('node_modules/react-dom') ||
+            id.includes('node_modules/react-router')
+          ) {
+            return 'vendor';
+          }
+        },
+      },
+    },
+  },
 });
